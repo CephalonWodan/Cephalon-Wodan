1ea4ae4638f7f9f4b9e92802972a35990c8b0402
Wodan <joanna.sage.pro@gmail.com>
2025-09-18 17:39:07 +0200

    Delete tools/wiki directory

--- tools/wiki/scrapers/WarframesModuleScraper.mjs
+++
@@ -1,9 +0,0 @@
-import WikiaDataScraper from '../WikiaDataScraper.mjs';
-import transformWarframe from '../transformers/transformWarframe.mjs';
-
-// On passe juste le titre du module ; la base construit RAW/EXPORT/EDIT
-export default class WarframesModuleScraper extends WikiaDataScraper {
-  constructor() {
-    super('Module:Warframes/data', 'Warframes', transformWarframe);
-  }
-}
