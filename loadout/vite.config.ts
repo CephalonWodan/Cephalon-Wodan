4b5a3ea9d454100b197fb256fd2fbc9f0affdffd
Wodan <joanna.sage.pro@gmail.com>
2025-11-03 21:07:47 +0100

    Delete loadout directory

--- loadout/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Configuration prévue pour GitHub Pages sous /Cephalon-Wodan/loadout/
-export default defineConfig({
-  base: '/Cephalon-Wodan/loadout/',
-  plugins: [react()],
-  optimizeDeps: { exclude: ['lucide-react'] },
-})
